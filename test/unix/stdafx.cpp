05edfd16716ac1c8b50a650c339798e6ae8240bb
ntake <sutezo9@me.com>
2014-12-10 18:33:27 +0900

    test/unix をWindowsコマンドラインにも対応した

---
+++ test/unix/stdafx.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+
+// ソース側で使用している TRACEマクロで、文字列に _T()をつけたりつけなかったりしたので
+
+
+void TRACE(const char* fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vprintf(fmt, ap);
+}
+
+void TRACE(const wchar_t* fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vwprintf(fmt, ap);
+}
+
