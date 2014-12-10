96bcb7e46e3844d52ba40679c7eccd396b3b1f46
TAKEDA, Noriyuki <sutezo666@gmail.com>
2014-12-10 18:33:27 +0900

    test/unix をWindowsコマンドラインにも対応した

--- test/unix/alpsmain.cpp
+++ test/unix/alpsmain.cpp
@@ -2,9 +2,17 @@
 
 extern int test_exec(void);
 
+int g_tax = 8;
+
 int main(int argc, char** argv)
 {
-	if (! DBS::getInstance()->open("../../db/jr.db")) {
+	_tsetlocale(LC_ALL, _T(""));	// tstring
+	
+	TRACE(_T("Hello\n"));
+	TRACE(_T("В±Вс\n"));
+	
+	
+	if (! DBS::getInstance()->open(_T("../../db/jr.db"))) {
 		printf("Can't db open\n");
 		return -1;
 	}
