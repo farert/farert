24ea6c5a19166777e4c90d84e6a948379e890587
sutezo <sutezo666@gmail.com>
2015-02-26 14:36:39 +0900

    北陸新幹線開業 DB update

--- db/mkdb.bat
+++ db/mkdb.bat
@@ -1,20 +1,9 @@
-@echo off
-rem jrdb.xls のlines, clinfar, rule69, rule70_new, rule86, fare2シートの
-rem 全内容(テキスト)を すべて jrdb.txt へ貼り付け
-rem 本バッチファイルを実行する
-
-python scripts\jr_db_reg.py
+call mkdbsub jrdb2014 2014
 if errorlevel 1 goto err
-python scripts\node_list.py >scripts\node_list.txt
+call mkdbsub jrdb2015 2015
 if errorlevel 1 goto err
-scripts\crypt_tofish.exe enc jr.db
-if errorlevel 1 goto err
-move jr.db.enc ..\app\win_mfc\fjr_mfc\alps_mfc\res
-if errorlevel 1 goto err
-rem popd
 @echo **** successfully *****
 goto fin
 :err
-rem popd
 @echo !!! failed !!!
 :fin
