229bf6807aaa78bf36c72f807a9548828f679ae1
TAKEDA, Noriyuki <sutezo666@gmail.com>
2015-07-08 20:39:10 +0900

    iOS Version 15.06 added.

---
+++ app/Farert/Farert/RouteSelStationSegue.swift
@@ -0,0 +1,68 @@
+//
+//  RouteSelStationSegue.swift
+//  iFarert
+//
+//  Created by TAKEDA, Noriyuki on 2015/03/23.
+//  Copyright (c) 2015年 TAKEDA, Noriyuki. All rights reserved.
+//
+
+import UIKit
+
+
+class RouteSelStationSegue: UIStoryboardSegue {
+
+    override func perform() {
+        
+//        if self.identifier == "autoRouteSegue" {
+//            /* 経路選択(路線選択)→最短経路 */
+//            let sourceViewController : UITableViewController = self.sourceViewController as! UITableViewController
+//            let destinationViewController : UINavigationController = self.destinationViewController as! UINavigationController
+//
+//            let navigationController : UINavigationController = sourceViewController.navigationController! as UINavigationController
+//
+//            //路線選択 - 最短経路
+//            navigationController.popViewControllerAnimated(false)
+//
+//            UIView.transitionWithView(navigationController.view, duration:0.3, options:UIViewAnimationOptions.TransitionCurlDown, animations: {
+//                navigationController.pushViewController(destinationViewController, animated: false)
+//                }, completion: nil)
+//            return
+//        }
+// iOS7.1で飛ぶので辞めました.  CustomでなくPresent Modallyにしました.
+        
+        let sourceViewController : CSTableViewController = self.sourceViewController as! CSTableViewController
+        let destinationViewController : CSTableViewController = self.destinationViewController as! CSTableViewController
+        
+        let navigationController : UINavigationController = sourceViewController.navigationController! as UINavigationController
+        
+        if (destinationViewController.transit_state == FGD.FA_TRANSIT_AUTOROUTE) {
+            destinationViewController.modalTransitionStyle = UIModalTransitionStyle.PartialCurl;
+            sourceViewController.presentViewController(destinationViewController, animated:true, completion:nil)
+            
+        } else if (destinationViewController.transit_state == FGD.FA_TRANSIT_HISTORY) {
+            /* 履歴 */
+            UIView.transitionWithView(navigationController.view, duration:0.3, options:UIViewAnimationOptions.TransitionCrossDissolve,
+                animations: {
+                 navigationController.pushViewController(destinationViewController, animated: false)
+                }, completion: nil)
+        } else {
+            if (destinationViewController.transit_state == FGD.FA_TRANSIT_STA2JCT) {
+                // Unwind segueの場合は、右からflipするアニメーション
+                // 着駅→分岐駅
+                navigationController.popViewControllerAnimated(false)
+                UIView.transitionWithView(navigationController.view, duration:0.3, options:UIViewAnimationOptions.TransitionFlipFromRight, animations: {
+                    navigationController.pushViewController(destinationViewController, animated: false)
+                    }, completion: nil)
+                //[navigationController popViewControllerAnimated:NO];
+                
+            } else {    // segueの場合は、左からflipするアニメーション
+                // 分岐駅→着駅
+                navigationController.popViewControllerAnimated(false)
+                UIView.transitionWithView(navigationController.view, duration:0.3, options:UIViewAnimationOptions.TransitionFlipFromLeft, animations: {
+                     navigationController.pushViewController(destinationViewController, animated: false)
+                    }, completion: nil)
+                //[navigationController popViewControllerAnimated:NO];
+            }
+        }
+    }
+}
