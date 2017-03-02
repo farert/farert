fbd4aafd7c1eff0171c3046a08be1232c367b619
sutezo <sutezo9@me.com>
2017-03-03 00:22:52 +0900

    会社線通過連絡運輸実装完(Win最終テスト, iOS実装未)

--- db/mkdball.bat
+++ db/mkdball.bat
@@ -1,11 +1,17 @@
+@echo off
+@echo ==== mkdb jrdb2014 2014 ====
 call mkdb jrdb2014 2014
 if errorlevel 1 goto err
+@echo ==== mkdb jrdb2015 2015 ====
 call mkdb jrdb2015 2015
 if errorlevel 1 goto err
+@echo ==== mkdb jrdb2016 2016 ====
 call mkdb jrdb2016 2016
 if errorlevel 1 goto err
-@echo **** successfully *****
+@echo **** complete *****
+@echo .
 goto fin
 :err
 @echo !!! failed !!!
 :fin
+pause
