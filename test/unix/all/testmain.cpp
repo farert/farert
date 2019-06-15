d8903e8cd61646e6c7dedb2d9f85a2516865af60
sutezo <sutezo666@gmail.com>
2019-06-16 00:06:41 +0900

    improve the test routine.

--- test/unix/all/testmain.cpp
+++ test/unix/all/testmain.cpp
@@ -1,8 +1,8 @@
-﻿#include "stdafx.h"
+#include "stdafx.h"
 
 extern int test_exec(void);
 extern void test_route(const TCHAR *route_def[], int round = 0);
-extern void test_autoroute(const TCHAR *route_def[]);
+extern void test_autoroute(const TCHAR *route_def[], int option = 0);
 static void from_stream(char* file, int option_num);
 
 int g_tax = 8;
@@ -32,15 +32,23 @@ const TCHAR* tr_a[] = { tbuf,  tbuf2, _T("") };
 void usage(const char* prgm)
 {
 	fprintf(stderr, "Usage: %s [-h|-num] [<path>|<station1 ... stationN>]\n", prgm);
-	fprintf(stderr, "      -h : show this.\n");
-	fprintf(stderr, "      -num : result format.\n");
-	fprintf(stderr, "             0: all\n");
-	fprintf(stderr, "             1: no return\n");
-	fprintf(stderr, "             2: no rule\n");
-	fprintf(stderr, "             3: no rule + no return.\n");
-	fprintf(stderr, "             4: no no_rule.\n");
-	fprintf(stderr, "             5: no no_rule + no return.\n");
-	fprintf(stderr, "             other: Non-available(default 0)\n");
+	fprintf(stderr, "      -exec : Execute the all test.\n");
+	fprintf(stderr, "      -num : (test_route).\n");
+	fprintf(stderr, "               result format.\n");
+	fprintf(stderr, "               0: all(default)\n");
+	fprintf(stderr, "               1: no return\n");
+	fprintf(stderr, "               2: no rule\n");
+	fprintf(stderr, "               3: no rule + no return.\n");
+	fprintf(stderr, "               4: no no_rule.\n");
+	fprintf(stderr, "               5: no no_rule + no return.\n");
+	fprintf(stderr, "               other: Non-available(default 0)\n");
+	fprintf(stderr, "           (auto route only).\n");
+	fprintf(stderr, "               1: zairaisen only\n");
+	fprintf(stderr, "               2: with shinkansen\n");
+	fprintf(stderr, "               3: with company line\n");
+	fprintf(stderr, "               4: with company line and shinkansen\n");
+	fprintf(stderr, "               0 or 4<: all(default)\n");
+	fprintf(stderr, "              +10: One result for rule apllied only.\n");
 	fprintf(stderr, "      path : route description file.\n");
 	fprintf(stderr, "      　　　　　If the first column is '#', it represents a comment.\n");
 	fprintf(stderr, "      　　　　　If the blank line, it's skip.\n");
@@ -60,28 +68,31 @@ int main(int argc, char** argv)
 	char* dbpath = getenv("farertDB");
 
 	if (!dbpath) {
-		printf("Should be set environment variable the 'farertDB' for use database.\n");
+		fprintf(stderr, "Should be set environment variable the 'farertDB' for use database.\n");
 		return -1;
 	}
 
 	if (!DBS::getInstance()->open(dbpath)) {
-		printf("Can't db open\n");
+		fprintf(stderr, "Can't db open\n");
 		return -1;
 	}
 	if (argc < 2) {
-		/* normal: execute all test pattern */
-		test_exec();
+		/* show usage */
+		usage(*argv);
+		return -1;
 	} else {
 		if ('-' == *(*(argv + 1))) {
 			/* Usage */
-			if (('h' == *((*(argv + 1)) + 1)) || ('H' == *((*(argv + 1)) + 1))) {
-				usage(*argv);
-				return -1;
+			if (0 == strncmp(*(argv + 1), "-exec", 5)) {
+				/* normal: execute all test pattern */
+				test_exec();
+				return 0;
+			} else {
+				/* result variation */
+				option_num = atoi((*(argv + 1)) + 1);
+				++argv;
+				--argc;
 			}
-			/* result variation */
-			option_num = atoi((*(argv + 1)) + 1);
-			++argv;
-			--argc;
 		}
 		if (argc == 2) {
 			/* route from read file */
@@ -104,7 +115,10 @@ int main(int argc, char** argv)
 			if ((argc % 2) == 0) {
 				test_route(tr, option_num);
 			} else {
-				test_autoroute(tr);
+				// option_num  0 or 5~9 線パターン  1 在来線のみ 2 新幹線使う 3 会社線もつかう 4 新幹線も会社線もつかう
+				//        +10 特例適用のみ表示
+
+				test_autoroute(tr_a, option_num);
 			}
 		}
 	}
@@ -124,9 +138,61 @@ static void rtrim(char* str)
 	}
 }
 
+int num_of_word(const char* buf)
+{
+	char* p;
+	const char* sep = " \t";
+	int c = 0;
+	char* str = (char*)malloc(strlen(buf) + 1);
+
+	if (NULL == str) {
+		return 0;
+	}
+
+	strcpy(str, buf);
+
+	p = strtok(str, sep);
+	while (p != NULL) {
+		c++;
+		p = strtok(NULL, sep);
+	}
+	free(str);
+	return c;
+}
+
+// numワードめの文字列ポインタを返す. numは1~Nで、1ならstrを返す
+const char* subword(const char* src_str, int num)
+{
+	char* p;
+	const char* sep = " \t";
+	int c = 0;
+	char* str = (char*)malloc(strlen(src_str) + 1);
+
+	if (NULL == str) {
+		return src_str + strlen(src_str); // empty string
+	}
+
+	strcpy(str, src_str);
+
+	p = strtok(str, sep);
+	while (p != NULL) {
+		c++;
+		if (num <= c) {
+			return src_str + (p - str);
+		}
+		p = strtok(NULL, sep);
+	}
+	free(str);
+	return src_str + strlen(src_str); // empty
+}
+
+
 static void from_stream(char* file, int option_num)
 {
 	FILE* fp;
+	int c;
+	const char* lp;
+
 	fp = fopen(file, "r");
 	if (fp == NULL) {
 		fprintf(stderr, "Can't open file: %s\n", file);
@@ -139,7 +205,15 @@ static void from_stream(char* file, int option_num)
 		} else if ('/' == tbuf[0]) {
 			break;
 		}
-		test_route(tr, option_num);
+		c = num_of_word(tbuf);
+		if ((c % 2) != 0) {
+			test_route(tr, option_num);
+		} else {
+			lp = subword(tbuf, c);
+			strcpy(tbuf2, lp);
+			tbuf[lp - tbuf] = '\0';
+			test_autoroute(tr_a, option_num);
+		}
 	}
 	fclose(fp);
 }
