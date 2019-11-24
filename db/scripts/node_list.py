e5e46b018af9b5d0347d192fc5433a8205b5d2ad
sutezo <sutezo666@gmail.com>
2019-11-24 22:42:43 +0900

    C++/Java: 大都市近郊区間の運賃計算を適正化(大船-拝島や新横浜-福生など)

--- db/scripts/node_list.py
+++ db/scripts/node_list.py
@@ -27,10 +27,24 @@ else:
 jrdb.con = sqlite3.connect(dbfn)  # , isolation_level=None)
 
 
+def attributes(jctid1, jctid2):
+    attr = 0
+    station_id1 = jrdb.station_id_from_id(jctid1)
+    station_id2 = jrdb.station_id_from_id(jctid2)
+
+    if jrdb.is_densya_specific_line(station_id1, station_id2):
+        attr = 1
+                    # 電車をさきに訊かないと、拝島-八王子 などがあるので。
+    if jrdb.is_local_line(station_id1, station_id2):
+        attr = 2
+
+    return attr
+
+
 n = 0
 items = []
-for jct in jrdb.jct_list():
-	for node in jrdb.node_next(jct):
+for jct in jrdb.jct_list(): # 分岐駅リスト
+	for node in jrdb.node_next(jct):   # 分岐駅の隣り合うノード長
 		items.append([node[2], node[1], jct, node[0]]) # line, cost, jct, neer
 		# print(jrdb.station_from_jctid(jct), jrdb.station_from_jctid(node[0]), node[1], jrdb.line_name(node[2]))
 		n += 1
@@ -46,7 +60,7 @@ for item in items:
 	t_item = b_item.split('/')
 	if t_item[0] == str(item[0]) and t_item[1] == str(item[1]) and \
 	   t_item[3] == str(item[2]) and t_item[2] == str(item[3]):
-		# 除外するもの
+		# 除外するもの　: (jct, neer),(neer, jct)があるので、重複経路は覗く
 		#print(jrdb.station_from_jctid(item[2]), jrdb.station_from_jctid(item[3]), item[1], jrdb.line_name(item[0]))
 		pass
 	else:
@@ -69,19 +83,21 @@ create table t_node (
 	neer_id	integer not null references t_jct(id),
 	cost	integer not null,
 	line_id	integer not null references t_line(rowid),
+    attr    integer not null default (0),
 
 	primary key (jct_id, neer_id, line_id)
 	);
 """)
 ###########################################
-items = list(map(lambda x:[x[2], x[3], x[1], x[0]], items2))
+items = list(map(lambda x:[x[2], x[3], x[1], x[0], attributes(x[2], x[3])], items2))
 items.sort()
+# items = dict(zip(["jct", "neer", "cost", line_id"], items))
 for item in items:
 	print(jrdb.station_from_jctid(item[0]) + "\t" +
 		  jrdb.station_from_jctid(item[1]) + "\t" +
 		  str(item[2]) + "\t" + jrdb.line_name(item[3]))
 
-jrdb.con.executemany('insert into t_node values(?, ?, ?, ?)', items)
+jrdb.con.executemany('insert into t_node values(?, ?, ?, ?, ?)', items)
 jrdb.con.commit()
 print("registerd t_node {0} affected.".format(len(items)))
 assert(len(items2) == len(items))
