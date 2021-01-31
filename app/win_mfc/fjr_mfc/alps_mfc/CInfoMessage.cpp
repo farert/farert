aaaf4b2d6d86e0b33466476492434feafdce8c4b
sutezo <sutezo666@gmail.com>
2021-01-31 11:11:11 +0900

    Winodows CInfoMessageリポジトリ登録し忘れていた（19/10/22頃より) Windows b#20090901 fix.

---
+++ app/win_mfc/fjr_mfc/alps_mfc/CInfoMessage.cpp
@@ -0,0 +1,56 @@
+﻿// CInfoMessage.cpp : 実装ファイル
+//
+
+#include "stdafx.h"
+#include "alps_mfc.h"
+#include "CInfoMessage.h"
+#include "afxdialogex.h"
+
+
+// CInfoMessage ダイアログ
+
+IMPLEMENT_DYNAMIC(CInfoMessage, CDialogEx)
+
+CInfoMessage::CInfoMessage(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_INFOMSG, pParent)
+{
+
+}
+
+CInfoMessage::~CInfoMessage()
+{
+}
+
+void CInfoMessage::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CInfoMessage, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CInfoMessage メッセージ ハンドラー
+
+
+BOOL CInfoMessage::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	GetDlgItem(IDC_STATIC_TITLE)->SetWindowText(m_title);
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 例外 : OCX プロパティ ページは必ず FALSE を返します。
+}
+
+
+void CInfoMessage::OnOK()
+{
+	if (BST_CHECKED == ((CButton*)GetDlgItem(IDC_CHKNOMSG))->GetCheck()) {
+		CDialogEx::OnOK();
+	}
+	else {
+		CDialogEx::OnCancel();
+	}
+}
---
+++ app/win_mfc/fjr_mfc/alps_mfc/CInfoMessage.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+
+// CInfoMessage ダイアログ
+
+class CInfoMessage : public CDialogEx
+{
+	DECLARE_DYNAMIC(CInfoMessage)
+
+public:
+	CInfoMessage(CWnd* pParent = nullptr);   // 標準コンストラクター
+	virtual ~CInfoMessage();
+
+// ダイアログ データ
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_INFOMSG };
+#endif
+	void SetTitleMessage(const CString& str) { m_title = str; }
+private:
+	CString m_title;
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV サポート
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	virtual void OnOK();
+};
