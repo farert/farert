a50e4de0c953a50b3dc453874b32aca68b13d568
sutezo <sutezo666@gmail.com>
2021-12-31 14:37:27 +0900

    b#21110101F bugfix update test assert

--- app/Farert.android/app/src/prototype/java/org/sutezo/alps/farertAssert.java
+++ app/Farert.android/app/src/prototype/java/org/sutezo/alps/farertAssert.java
@@ -1,15 +1,17 @@
 package org.sutezo.alps;
 
+import java.util.Objects;
+
 public class farertAssert {
     static void ASSERT(boolean condition) {
         if (!condition) {
-            Thread.currentThread().getUncaughtExceptionHandler().uncaughtException(
+            Objects.requireNonNull(Thread.currentThread().getUncaughtExceptionHandler()).uncaughtException(
                     Thread.currentThread(), new AssertionError("RouteUtil.ASSERT !!!!!!"));
         }
     }
     static void ASSERT(boolean condition, String message) {
         if (!condition) {
-            Thread.currentThread().getUncaughtExceptionHandler().uncaughtException(
+            Objects.requireNonNull(Thread.currentThread().getUncaughtExceptionHandler()).uncaughtException(
                     Thread.currentThread(), new AssertionError("RouteUtil.ASSERT !!!!!!" + message));
         }
     }
