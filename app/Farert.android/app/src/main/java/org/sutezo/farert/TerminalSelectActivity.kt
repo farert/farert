fe4fe2e8f795db62f0f93378ecf078af897837fb
sutezo <sutezo666@gmail.com>
2022-04-23 22:45:02 +0900

    add android AlertDialog icon

--- app/Farert.android/app/src/main/java/org/sutezo/farert/TerminalSelectActivity.kt
+++ app/Farert.android/app/src/main/java/org/sutezo/farert/TerminalSelectActivity.kt
@@ -10,6 +10,7 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.content.ContextCompat
+import android.support.v4.content.res.ResourcesCompat
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
@@ -191,6 +192,7 @@ class TerminalSelectActivity : AppCompatActivity() {
                         saveHistory(context, listOf())
                     }
                     setNegativeButton("No", null)
+                    setIcon(ResourcesCompat.getDrawable(resources, R.drawable.ic_question_answer, null))
                     create()
                     show()
                 }
