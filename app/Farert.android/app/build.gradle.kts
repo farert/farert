50f6ab73a0f14b45db22b0df143abdd59383c576
Sutezo <sutezo666@gmail.com>
2025-11-16 13:48:23 +0900

    Release Android 25.11(38)

--- app/Farert.android/app/build.gradle.kts
+++ app/Farert.android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "org.sutezo.farert"
         minSdk = 24
         targetSdk = 36
-        versionCode = 37
-        versionName = "25.09"
+        versionCode = 38
+        versionName = "25.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
