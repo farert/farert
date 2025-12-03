e6d03b333d80d1f05712366437317abeb035c478
Sutezo <sutezo666@gmail.com>
2025-12-03 19:57:01 +0900

    Fix Convert string begin/endStationId.
    - azusa.cpp

--- app/alps/azusa.cpp
+++ app/alps/azusa.cpp
@@ -129,8 +129,8 @@ std::string az_route::get_fare_info_object_json() {
         json_encoder::pair("isResultCompanyBeginEnd", fi.isBeginEndCompanyLine()),
         json_encoder::pair("isResultCompanyMultipassed", fi.isMultiCompanyLine()),
         json_encoder::pair("isEnableTokaiStockSelect", fi.isEnableTokaiStockSelect()),
-        json_encoder::pair("beginStationId", fi.getBeginTerminalId()),
-        json_encoder::pair("endStationId", fi.getEndTerminalId()),
+        json_encoder::pair("beginStation", CalcRoute::BeginOrEndStationName(fi.getBeginTerminalId())),
+        json_encoder::pair("endStationId", CalcRoute::BeginOrEndStationName(fi.getEndTerminalId())),
         json_encoder::pair("isBeginInCity", FARE_INFO::IsCityId(fi.getBeginTerminalId())),
         json_encoder::pair("isEndInCity", FARE_INFO::IsCityId(fi.getEndTerminalId())),
         /* Rule114 */
