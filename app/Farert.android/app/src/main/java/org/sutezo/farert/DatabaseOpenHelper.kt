25313f430fc7ee57c19a95269c9591b73fc84fa2
Sutezo <sutezo666@gmail.com>
2026-05-03 19:19:15 +0900

    Release 26.05
    - Windows 2.1.0.25
    - iOS/macOS 94
    - Android 49

--- app/Farert.android/app/src/main/java/org/sutezo/farert/DatabaseOpenHelper.kt
+++ app/Farert.android/app/src/main/java/org/sutezo/farert/DatabaseOpenHelper.kt
@@ -17,7 +17,7 @@ class DatabaseOpenHelper(context: Context) : SQLiteOpenHelper(context, DB_NAME, 
     companion object {
         const val DB_NAME = "jrdb.db"
         //val DB_NAME_ASSET = "routeDB/jrdb2017.db"
-        const val DATABASE_VERSION = 36    // 36=2026.4.20  // !!! DB更新したらDATABASE_VERSION を更新
+        const val DATABASE_VERSION = 37    // 37=2026.5.3  // !!! DB更新したらDATABASE_VERSION を更新
         const val DEFAULT_DB_IDX = 5  // "2025"
         private const val MIN_DB_IDX = 0
         private const val MAX_DB_IDX = 5
